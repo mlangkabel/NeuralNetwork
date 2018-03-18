2e2988c52269022867e2d0fbde1559a2b62723ac
mlangkabel <malte.langkabel@gmail.com>
2018-03-18 12:14:33 +0100

    add random co-mutation of multiple features

--- src/app/NeuroEvolutionEnvironment.cpp
+++ src/app/NeuroEvolutionEnvironment.cpp
@@ -40,17 +40,28 @@ NeuralNetworkGenotype NeuroEvolutionEnvironment::crossover(const NeuralNetworkGe
 
 NeuralNetworkGenotype NeuroEvolutionEnvironment::mutate(const NeuralNetworkGenotype& genotype)
 {
+	WeightedRandomGenerator<int> countRng(utility::getRandomInt(0, 10000));
+	countRng.addResult(1, 1.0f / 2.0f);
+	countRng.addResult(2, 1.0f / 4.0f);
+	countRng.addResult(3, 1.0f / 8.0f);
+	countRng.addResult(4, 1.0f / 16.0f);
+	countRng.addResult(5, 1.0f / 32.0f);
+
+	const int mutationCount = countRng.getResult();
 	NeuralNetworkGenotype mutatedGenotype = genotype;
 
-	WeightedRandomGenerator<Matrix<float>*> wrng(utility::getRandomInt(0, 10000));
-	wrng.addResult(&mutatedGenotype.inputToHiddenNodeWeights, mutatedGenotype.inputToHiddenNodeWeights.getElementCount());
-	wrng.addResult(&mutatedGenotype.hiddenToHiddenNodeWeights, mutatedGenotype.hiddenToHiddenNodeWeights.getElementCount());
-	wrng.addResult(&mutatedGenotype.hiddenToOutputNodeWeights, mutatedGenotype.hiddenToOutputNodeWeights.getElementCount());
+	WeightedRandomGenerator<Matrix<float>*> weightsRng(utility::getRandomInt(0, 10000));
+	weightsRng.addResult(&mutatedGenotype.inputToHiddenNodeWeights, mutatedGenotype.inputToHiddenNodeWeights.getElementCount());
+	weightsRng.addResult(&mutatedGenotype.hiddenToHiddenNodeWeights, mutatedGenotype.hiddenToHiddenNodeWeights.getElementCount());
+	weightsRng.addResult(&mutatedGenotype.hiddenToOutputNodeWeights, mutatedGenotype.hiddenToOutputNodeWeights.getElementCount());
 
-	Matrix<float>* weights = wrng.getResult();
+	for (int i = 0; i < mutationCount; i++)
+	{
+		Matrix<float>* weights = weightsRng.getResult();
 
-	const int indexToMutate = utility::getRandomInt(0, weights->getElementCount() - 1);
-	(*weights)[indexToMutate] += utility::getRandomFloat(-0.1f, 0.1f);
+		const int indexToMutate = utility::getRandomInt(0, weights->getElementCount() - 1);
+		(*weights)[indexToMutate] += utility::getRandomFloat(-0.1f, 0.1f);
+	}
 
 	return mutatedGenotype;
 }
